28b79b8876ed8875c8f22f227313f4565b5e504c
Charles Zhang <szinubuntu@gmail.com>
2022-10-14 12:27:21 -0400

    Add project files.

---
+++ AutomationHelper.cs
@@ -0,0 +1,107 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Octave_
+{
+    [Obsolete("Not useful; Use System.Windows.Forms instead", true)]
+    public static class AutomationHelper
+    {
+        #region Windows API
+        /// <summary>
+        /// Struct representing a point, for Windows API.
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public static implicit operator Point(POINT point)
+            {
+                return new Point(point.X, point.Y);
+            }
+        }
+        /// <summary>
+        /// Retrieves the cursor's position, in screen coordinates.
+        /// </summary>
+        /// <see>See MSDN documentation for further information.</see>
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+        /// <summary>
+        /// Not working
+        /// </summary>
+        [DllImport("user32.dll")]
+        public static extern bool SetCursorPos(in int x, in int y);
+        [StructLayout(LayoutKind.Sequential)]
+        struct MOUSEINPUT
+        {
+            int dx;
+            int dy;
+            int mouseData;
+            public int dwFlags;
+            int time;
+            IntPtr dwExtraInfo;
+        }
+        struct INPUT
+        {
+            public uint dwType;
+            public MOUSEINPUT mi;
+        }
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint SendInput(uint cInputs, INPUT input, int size);
+        #endregion
+
+        #region Helpers
+        public static Point GetCursorPosition()
+        {
+            POINT lpPoint;
+            GetCursorPos(out lpPoint);
+            // NOTE: If you need error handling
+            // bool success = GetCursorPos(out lpPoint);
+            // if (!success)
+
+            return lpPoint;
+        }
+        public static void SetCursorPosition(Point position)
+        {
+            var input = new INPUT()
+            {
+                dwType = 0, // Mouse input
+                mi = new MOUSEINPUT() { dwFlags = 0x2 }
+            };
+
+            if (SendInput(1, input, Marshal.SizeOf(input)) == 0)
+            {
+                throw new Exception();
+            }
+        }
+        #endregion
+
+        #region Examples
+        public static void ExampleClick()
+        {
+            DoClickMouse(0x2); // Left mouse button down
+            DoClickMouse(0x4); // Left mouse button up
+
+            void DoClickMouse(int mouseButton)
+            {
+                var input = new INPUT()
+                {
+                    dwType = 0, // Mouse input
+                    mi = new MOUSEINPUT() { dwFlags = mouseButton }
+                };
+
+                if (SendInput(1, input, Marshal.SizeOf(input)) == 0)
+                {
+                    throw new Exception();
+                }
+            }
+        }
+        public static void ExampleGet()
+        {
+            Point p = GetCursorPosition();
+            Console.WriteLine($"X: {p.X}, Y: {p.Y}");
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Octave_;
+using System.Drawing;
+using Console = Colorful.Console;
+using System.Windows.Forms;
+
+// Print welcome
+Console.WriteLine(
+@"
+GNU Octave, version 7.2.0
+Copyright (C) 1993-2022 The Octave Project Developers.
+This is free software; see the source code for copying conditions.
+There is ABSOLUTELY NO WARRANTY; not even for MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  For details, type 'warranty'.
+
+Octave was configured for ""x86_64-w64-mingw32"".
+
+Additional information about Octave is available at https://www.octave.org.
+
+Please contribute if you find this software useful.
+For more information, visit https://www.octave.org/get-involved.html
+
+Read https://www.octave.org/bugs.html to learn how to submit bug reports.
+For information about changes from previous versions, type 'news'.
+
+".TrimStart());
+
+// Read pescudo scripts
+string scriptFile = "Scripts.txt";
+if (!File.Exists(scriptFile))
+{
+    Console.Write($"Missing: {scriptFile}");
+    return;
+}
+string[] scripts = File.ReadAllLines(scriptFile);
+
+Point lastCursorPos = new Point(0, 0);
+Cursor.Position = lastCursorPos;
+
+int commandIndex = 0;
+while (true)
+{
+    Console.Write($"octave:{commandIndex}> ", Color.Green);
+    Console.WriteLine(scripts[(commandIndex * 2) % scripts.Length], Color.White);
+    Thread.Sleep(new Random().Next(150, 1500));
+
+    Console.WriteLine(scripts[(commandIndex * 2 + 1) % scripts.Length], Color.White);
+    Thread.Sleep(new Random().Next(150, 1500));
+
+    if (Cursor.Position != lastCursorPos)
+        return;
+    Point newCursorPos = new Point(new Random().Next(1024), new Random().Next(768));
+    Cursor.Position = newCursorPos;
+    lastCursorPos = newCursorPos;
+
+    commandIndex++;
+}
